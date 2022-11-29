b7a9523998e52019f2f0ae0c5a696608fed3a88a
Sergio Loza <a2020110121@estudiantes.upsa.edu.bo>
2022-11-28 22:09:36 -0400

    Intento final

--- packages/flutter/lib/src/widgets/framework.dart
+++
@@ -1,1 +0,0 @@
-No Content: http://localhost:35049/packages/flutter/lib/src/widgets/framework.dart
--- packages/my_ticket_care/src/login/providers/user_data_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../shared/models/person.dart';
-
-final userDataProvider = StateProvider<Person>((ref) {
-  return Person.empty();
-});
